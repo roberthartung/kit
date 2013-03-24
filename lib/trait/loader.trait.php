e1d395b6e1269b24ecc2ab8006bcb62fd0dc62cd
Robert Hartung <r.hartung@roberthartung.de>
2013-03-24 09:11:55 +0100

    Some changes to the loader Trait

--- lib/trait/loader.trait.php
+++ lib/trait/loader.trait.php
@@ -1,18 +1,18 @@
 <?php
 	namespace kit;
 	
-	use kit\kit;
-	use kit\template\parser;
-	
 	trait loaderTrait
 	{
 		private $kit;
 		
 		private $template_parser;
 		
+		private $cfg;
+		
 		public function __construct()
 		{
 			$this->kit = kit::getInstance();
+			$this->cfg = cfg::getInstance();
 			$this->template_parser = template\parser::getInstance();
 		}
 	}
